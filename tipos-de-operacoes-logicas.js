14f738675f49490a0f3f6862604f6eaca7c69a91
leonardo-hlr <leonardohlr98@gmail.com>
2024-06-07 10:24:16 -0300

    Removidos os comentários do fonte.

--- tipos-de-operacoes-logicas.js
+++ tipos-de-operacoes-logicas.js
@@ -1,24 +1,3 @@
-/*
-passou := falso
-    escreval("Digite o nome do aluno")
-    leia(nome)
-    escreval("Digite a primeira nota")
-    leia(nota1)
-    escreval("Digite a segunda nota")
-    leia(nota2)
-    media := (nota1+nota2)/2
-
-    se media >= 5 entao
-       passou := verdadeiro
-    fimse
-    
-    //se (passou) e (media >=5 ou media <=7) entao
-    se (passou) e (nome == k) entao
-       escreval("Aprovado com média ",media)
-    senao
-       escreval("Reprovado com média ",media)
-    fimse
-    */
 
     var nome, nota1, nota2, media, passou;
     passou = false;
